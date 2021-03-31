771a00830fc6e5d3e94e629fd97995dea8f6a347
soohoonlee <jjinimania@gmail.com>
2021-04-01 00:35:56 +0900

    Initial Commit

---
+++ src/main/kotlin/io/github/soohoonlee/board/BoardApplication.kt
@@ -0,0 +1,11 @@
+package io.github.soohoonlee.board
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BoardApplication
+
+fun main(args: Array<String>) {
+    runApplication<BoardApplication>(*args)
+}
